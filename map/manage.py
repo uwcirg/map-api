2514869e485914837541274d3ab3a8b2b576c710
Ivan Cvitkovic <ivanc@uw.edu>
2020-04-06 13:51:58 -0700

    Add flask sync command

--- map/manage.py
+++ map/manage.py
@@ -1,40 +1,12 @@
 import click
-from flask.cli import FlaskGroup
 
 from map.app import create_app
 
 
-def create_map(info):
-    return create_app(cli=True)
+app = create_app()
 
 
-@click.group(cls=FlaskGroup, create_app=create_map)
-def cli():
-    """Main entry point"""
-
-
-@cli.command("init")
-def init():
-    """Init application, create database tables
-    and create a new user named admin with password admin
-    """
-    from map.extensions import db
-    from map.models import User
-    click.echo("create database")
-    db.create_all()
-    click.echo("done")
-
-    click.echo("create user")
-    user = User(
-        username='admin',
-        email='admin@mail.com',
-        password='admin',
-        active=True
-    )
-    db.session.add(user)
-    db.session.commit()
-    click.echo("created user admin")
-
-
-if __name__ == "__main__":
-    cli()
+@app.cli.command("sync")
+def sync():
+    """Load static data idempotently"""
+    click.echo('sync CLI command complete')
