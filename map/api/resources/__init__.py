a1a0c292e9905a4af4290b4846c8849b26ae2e67
pbugni <pbugni@u.washington.edu>
2020-05-06 11:54:25 -0700

    Feature/debug migration (#33)
    * Adding printf debugging to migration process.
    
    * remove obsolete code (from cookie-cutter)
    
    * freeze requirements to resolve differences between installs; remove obsolete code (from cookie-cutter)

--- map/api/resources/__init__.py
+++ map/api/resources/__init__.py
@@ -1,11 +1,8 @@
 from .fhir_resource import FhirResource, FhirSearch
 from .sync import Sync
-from .user import UserResource, UserList
 
 __all__ = [
     'FhirSearch',
     'FhirResource',
     'Sync',
-    'UserResource',
-    'UserList'
 ]
--- map/api/views.py
+++ map/api/views.py
@@ -4,9 +4,6 @@ from flask_restful import Api
 from map.api.resources import (
     FhirResource,
     FhirSearch,
-    Sync,
-    UserResource,
-    UserList,
 )
 from map.config import API_PREFIX
 
@@ -17,8 +14,3 @@ api = Api(blueprint)
 
 api.add_resource(FhirResource, '/<string:resource_type>/<int:resource_id>')
 api.add_resource(FhirSearch, '/<string:resource_type>')
-
-# Removing following views until adequately secured
-#api.add_resource(Sync, '/sync/<string:resource_type>')
-#api.add_resource(UserResource, '/users/<int:user_id>')
-#api.add_resource(UserList, '/users')
